21f95d774745ee3f4647582cb87465238e311065
muhammadzain8 <muhammadzain8@gmail.com>
2022-02-07 19:49:38 +0500

    1 new fix

--- src/dialogs/UserModal.js
+++ src/dialogs/UserModal.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
 
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 import { v4 as uuid } from 'uuid';
 import { makeStyles } from '@material-ui/styles';
 
